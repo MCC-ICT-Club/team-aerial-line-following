65a3093df88a0ff29f385f98b081bd82cc7b5b5e
saiyajinsolis <saiyajinsolis@gmail.com>
2024-11-11 21:23:51 -0500

    Introduced Exposure controls into the color picker program. To provide a means to track and maintain a static set of exposure values reducing reliance on auto adjustments of camera settings and add more versatility in programs use within varying settings and conditions.

--- ColorPicker.py
+++ ColorPicker.py
@@ -9,6 +9,43 @@ frameHeight = 360
 #me.connect()
 #print(me.get_battery())
 
+camNum = 0
+bufferSize = 1
+captureProperties = [
+    cv.CAP_PROP_AUTO_EXPOSURE, 
+    cv.CAP_PROP_ISO_SPEED,
+    cv.CAP_PROP_AUTO_WB,
+    cv.CAP_PROP_HUE,
+    cv.CAP_PROP_GAMMA,
+    cv.CAP_PROP_AUTOFOCUS,
+    cv.CAP_PROP_EXPOSURE, 
+    cv.CAP_PROP_BRIGHTNESS,    
+    cv.CAP_PROP_GAIN, 
+    cv.CAP_PROP_SATURATION,
+    cv.CAP_PROP_TEMPERATURE,
+    cv.CAP_PROP_CONTRAST,
+    cv.CAP_PROP_WB_TEMPERATURE,
+    cv.CAP_PROP_GUID,
+    cv.CAP_PROP_IRIS,
+    cv.CAP_PROP_BACKLIGHT,
+    cv.CAP_PROP_TRIGGER,
+    cv.CAP_PROP_MONOCHROME,
+    cv.CAP_PROP_FPS,
+    cv.CAP_PROP_MODE,
+    cv.CAP_PROP_RECTIFICATION,
+    cv.CAP_PROP_SHARPNESS,
+    cv.CAP_PROP_FOCUS,
+    cv.CAP_PROP_SETTINGS,
+    cv.CAP_PROP_CODEC_PIXEL_FORMAT,
+    cv.CAP_PROP_BITRATE,
+    cv.CAP_PROP_FOURCC,
+    cv.CAP_PROP_VIDEO_STREAM,
+    cv.CAP_PROP_CHANNEL,
+]
+
+trackBarNames = []
+
+
 
 def empty(a):
     pass
@@ -22,12 +59,241 @@ cv.createTrackbar("SAT Max", "HSV", 255, 255, empty)
 cv.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
 cv.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
 
-cap = cv.VideoCapture(0)
+cv.namedWindow("Lighting")
+cv.resizeWindow("Lighting", 640, 240)
+cv.createTrackbar("Exposure", "Lighting", 1500, 2000, empty)
+cv.createTrackbar("Brightness", "Lighting", 155, 255, empty)
+cv.createTrackbar("Gain", "Lighting", 75, 255, empty)
+cv.createTrackbar("Saturation", "Lighting", 50, 255, empty)
+cv.createTrackbar("Temperature", "Lighting", 0, 10000, empty)
+cv.createTrackbar("Constrast", "Lighting", 0, 255, empty)
+cv.createTrackbar("WB Temperature", "Lighting", 0, 10000, empty)
+cv.createTrackbar("GUID", "Lighting", -40,  40, empty)
+cv.createTrackbar("IRIS", "Lighting", -40, 40, empty)
+cv.createTrackbar("Backlight", "Lighting", 50, 255, empty)
+cv.createTrackbar("Trigger", "Lighting", 0, 255, empty)
+cv.createTrackbar("Monochrome", "Lighting", 0, 255, empty)
+cv.createTrackbar("FPS", "Lighting", 0, 30, empty)
+cv.createTrackbar("MODE", "Lighting", 0, 30, empty)
+
+cv.createTrackbar("Rectification", "Lighting", 0, 30, empty)
+cv.createTrackbar("Convert RGB", "Lighting", 0, 30, empty)
+cv.createTrackbar("Sharpness", "Lighting", 0, 30, empty)
+cv.createTrackbar("Focus", "Lighting", 0, 30, empty)
+cv.createTrackbar("Settings", "Lighting", 0, 30, empty)
+cv.createTrackbar("Channel", "Lighting", 0, 30, empty)
+cv.createTrackbar("Codec Pixel Format", "Lighting", 0, 30, empty)
+cv.createTrackbar("Bitrate", "Lighting", 0, 30, empty)
+cv.createTrackbar("Video Stream", "Lighting", 0, 30, empty)
+cv.createTrackbar("FourCC", "Lighting", 0, 30, empty)
+
+def initializeCamera(camNum):
+    cap = cv.VideoCapture(camNum, cv.CAP_V4L2)
+    cap.set(cv.CAP_PROP_BUFFERSIZE, bufferSize)
+    # cap.set(cv.CAP_PROP_CONTRAST, 255)
+    return cap
+
+def setCaptureProperty(cap, prop_id, value, prop_name):
+    cap.set(prop_id, value)
+    actual_value = cap.get(prop_id)
+    camProp = f'Property {prop_name} set to {actual_value}\n'
+    return camProp
+
+def appropriateCapPropValue(cap, prop_id):
+    propName = ""
+    match prop_id:
+        case 21:
+            propName = "Auto Exposure"
+            value = cap.get(captureProperties[0])
+            value = 1
+        case 30:
+            propName = "ISO Speed"
+            value = cap.get(captureProperties[1])
+            value = 1
+        case 44:
+            propName = "Auto WB"
+            value = cap.get(captureProperties[2])
+            value = 3
+        case 13:
+            propName = "Hue"
+            value = cap.get(captureProperties[3])
+            value = 2.0
+        case 22:
+            propName = "Gamma"
+            value = cap.get(captureProperties[4])
+            value = 3.0    
+        case 39:
+            propName = "Autofocus"
+            value = cap.get(captureProperties[5])
+            value = 3
+        case 15:
+            propName = "Exposure"
+            exposure    = cv.getTrackbarPos("Exposure", "Lighting")
+            value = exposure
+        case 10:
+            propName = "Brightness"
+            brightness  = cv.getTrackbarPos("Brightness", "Lighting")
+            value = brightness
+        case 14:
+            propName = "Gain"
+            gain  = cv.getTrackbarPos("Gain", "Lighting")
+            value = gain
+        case 12: 
+            propName = "Saturation"
+            saturation  = cv.getTrackbarPos("Saturation", "Lighting")
+            value = saturation
+        case 23:
+            propName = "Temperature"
+            temperature = cv.getTrackbarPos("Temperature", "Lighting")
+            value = temperature
+        case 11:
+            propName = "Contrast"
+            contrast = cv.getTrackbarPos("Contrast", "Lighting")
+            value = contrast
+        case 45:
+            propName = "WB Temperature"
+            wb_temperature = cv.getTrackbarPos("WB Temperature", "Lighting")
+            value = wb_temperature
+        case 29:
+            propName = "GUID"
+            guid = cv.getTrackbarPos("GUID", "Lighting")
+            value = guid
+        case 36:
+            propName = "IRIS"
+            iris = cv.getTrackbarPos("IRIS", "Lighting")
+            value = iris
+        case 32:
+            propName = "Backlight"
+            backlight = cv.getTrackbarPos("Backlight", "Lighting")
+            value = backlight
+        case 24:
+            propName = "Trigger"
+            trigger = cv.getTrackbarPos("Trigger", "Lighting")
+            value = trigger
+        case 19:
+            propName = "Monochrome"
+            monochrome = cv.getTrackbarPos("Monochrome", "Lighting")
+            value = monochrome
+        case 5:
+            propName = "FPS"
+            fps = cv.getTrackbarPos("FPS", "Lighting")
+            value = fps
+        case 9:
+            propName = "MODE"
+            mode = cv.getTrackbarPos("MODE", "Lighting")
+            value = mode
+        case 18:
+            propName = "Rectification"
+            rectification = cv.getTrackbarPos("Rectification", "Lighting")
+            value = rectification
+        case 16:
+            propName = "Convert RGB"
+            converRGB = cv.getTrackbarPos("Convert RGB", "Lighting")
+            value = converRGB
+        case 20:
+            propName = "Sharpness"
+            sharpness = cv.getTrackbarPos("Sharpness", "Lighting")
+            value = sharpness
+        case 28:
+            propName = "Focus"
+            focus = cv.getTrackbarPos("Focus", "Lighting")
+            value = focus
+        case 37:
+            propName = "Settings"
+            settings = cv.getTrackbarPos("Settings", "Lighting")
+            value = settings
+        case 43:
+            propName = "Channel"
+            channel = cv.getTrackbarPos("Channel", "Lighting")
+            value = channel
+        case 46:
+            propName = "Code Pixel Format"
+            pixelFormat = cv.getTrackbarPos("Code Pixel Format", "Lighting")
+            value = pixelFormat
+        case 47:
+            propName = "Bitrate"
+            bitrate = cv.getTrackbarPos("Bitrate", "Lighting")
+            value = bitrate
+        case 58:
+            propName = "Video Stream"
+            videoStream = cv.getTrackbarPos("Video Stream", "Lighting")
+            value = videoStream
+        case 6:
+            propName = "FourCC"
+            fourCC = cv.getTrackbarPos("FourCC", "Lighting")
+            value = fourCC
+        case _:
+            propName = "" 
+            value = None
+
+    return value, propName
+
+cap = initializeCamera(camNum)
 frameCounter = 0
+
+def createLightingWindow():
+    count = 0
+    newList = []
+    nameList = []
+    newCapPropList = []
+    nonAdjustablePropData = open("nonAdjustableProperties.txt", 'w')
+    print(f'{newCapPropList}')
+    for capProp in captureProperties:
+        if cap.get(capProp) == -1.0:
+            poppedItem = captureProperties[count]
+            newList.append(poppedItem)
+        
+        if count == (len(captureProperties) -1):
+            count = 0
+        
+        count += 1
+
+    
+    
+    count = 0
+    counter = 0
+    cv.namedWindow("Lighting")
+    cv.resizeWindow("Lighting", 640, 240)
+    for capProp in captureProperties:
+        value, propName = appropriateCapPropValue(cap, capProp)
+        print(f'Count: {count}\n')
+        print(f'Current Cap Prop: {capProp}')
+
+        if value != -1:
+            popItem = captureProperties[counter]
+            newCapPropList.append(popItem)
+
+        # if capProp == newList[count]:
+        #     print(f'Cap Prop: {capProp} \nNew List: {newList[count]}')
+        #     newCapPropList.remove(capProp)
+        
+        if count == (len(newList) -1):
+            count = 0
+
+        if count == (len(captureProperties) -1):
+            count = 0
+
+        count += 1
+        counter += 1
+
+    for capProp in newCapPropList:
+        value, propName = appropriateCapPropValue(cap, capProp)
+        cv.createTrackbar(propName, "Lighting", 0, 255, empty)
+        if capProp == 23:
+            cv.createTrackbar(propName, "Lighting", 0, 7000, empty)
+        elif capProp == 45:
+            cv.createTrackbar(propName, "Lighting", 0, 7000, empty)
+
+    nonAdjustablePropData.write(f'{newList}\n')
+    nonAdjustablePropData.write(f'{nameList}\n')
+    nonAdjustablePropData.write(f'{captureProperties}\n')
+    nonAdjustablePropData.write(f'{newCapPropList}\n')
+        
+# createLightingWindow()
 
 while True:
     #img = me.get_frame_read().frame
-
+    
     success, img = cap.read()
     img = cv.resize(img, (frameWidth, frameHeight))
     #img = cv.flip(img, 0)
@@ -40,11 +306,21 @@ while True:
     v_min = cv.getTrackbarPos("VALUE Min", "HSV")
     v_max = cv.getTrackbarPos("VALUE Max", "HSV")
 
+    cap.set(cv.CAP_PROP_CONTRAST, 80)
+
+    camPropData = open("camPropData.txt", 'w')
+    
+    for capProp in captureProperties:
+        cap.set(cv.CAP_PROP_CONTRAST, 55)
+        value, propName = appropriateCapPropValue(cap, capProp)
+        camProp = setCaptureProperty(cap, capProp, value, propName)
+        camPropData.write(camProp)
+
     lower = np.array([h_min, s_min, v_min])
     upper = np.array([h_max, s_max, v_max])
     mask  = cv.inRange(imgHsv, lower, upper)
     result = cv.bitwise_and(img, img, mask=mask)
-    print(f'[{h_min}, {s_min}, {v_min}, {h_max}, {s_max}, {v_max}]')
+    # print(f'[{h_min}, {s_min}, {v_min}, {h_max}, {s_max}, {v_max}]')
 
     mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
     hStack = np.hstack([img, mask, result])
